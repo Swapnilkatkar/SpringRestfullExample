50bcfe6d0b73fd6335ddea8e41fa786ef30c3713
Swapnilkatkar <33285675+Swapnilkatkar@users.noreply.github.com>
2017-11-02 19:41:11 +0530

    Dummy Changes added to StudentController class
    Extra Method added to StudentController class

--- src/controller/StudentController.java
+++ src/controller/StudentController.java
@@ -43,4 +43,19 @@ public class StudentController {
 		list.add(s5);
 		return list;
 	}
+	
+	public List<Student> DummycreateStudentList() {
+		Student s1 = new Student(1, "Swapnil");
+		Student s2 = new Student(2, "Sumit");
+		Student s3 = new Student(3, "Pankaj");
+		Student s4 = new Student(4, "Gaurav");
+		Student s5 = new Student(5, "S Koli");
+		List<Student> list = new ArrayList<Student>();
+		list.add(s1);
+		list.add(s2);
+		list.add(s3);
+		list.add(s4);
+		list.add(s5);
+		return list;
+	}
 }
